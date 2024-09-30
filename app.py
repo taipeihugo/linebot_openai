cd5c23aa96e5476069e2ab29666a27741dcedd2f
taipeihugo <taipeihugo@users.noreply.github.com>
2024-10-01 00:20:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -49,36 +49,103 @@ def earth_quake():
         result = ['抓取失敗...', '']  # 如果發生錯誤，返回失敗訊息
     return result
     
-# 取得某個地點的氣象資訊
+# 取得某個地點的氣象資訊，整合氣象預報與空氣品質
 def weather(address):
+    result = {}
+    code = '你的氣象 token'
+    # 即時天氣
     try:
         url = [f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization={code}',
             f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization={code}']
-        result = {}
         for item in url:
             req = requests.get(item)   # 爬取目前天氣網址的資料
             data = req.json()
-            station = data['records']['Station']   # 觀測站
+            station = data['records']['Station']
             for i in station:
-                city = i['GeoInfo']['CountyName']  # 縣市
-                area = i['GeoInfo']['TownName']    # 區域
-                # 使用「縣市+區域」作為 key，例如「高雄市前鎮區」就是 key
-                # 如果 result 裡沒有這個 key，就記錄相關資訊
+                city = i['GeoInfo']['CountyName']
+                area = i['GeoInfo']['TownName']
                 if not f'{city}{area}' in result:
                     weather = i['WeatherElement']['Weather']
-                    temp = i['WeatherElement']['AirTemperature'] 
+                    temp = i['WeatherElement']['AirTemperature']
                     humid = i['WeatherElement']['RelativeHumidity']
-                    # 回傳結果
                     result[f'{city}{area}'] = f'目前天氣狀況「{weather}」，溫度 {temp} 度，相對濕度 {humid}%！'
+    except:
+        pass
 
-        output = '找不到氣象資訊'
-        for i in result:
-            if i in address: # 如果地址裡存在 key 的名稱
-                output = f'「{address}」{result[i]}'
-                break
+
+    # 氣象預報
+    api_list = {"宜蘭縣":"F-D0047-001","桃園市":"F-D0047-005","新竹縣":"F-D0047-009","苗栗縣":"F-D0047-013",
+        "彰化縣":"F-D0047-017","南投縣":"F-D0047-021","雲林縣":"F-D0047-025","嘉義縣":"F-D0047-029",
+        "屏東縣":"F-D0047-033","臺東縣":"F-D0047-037","花蓮縣":"F-D0047-041","澎湖縣":"F-D0047-045",
+        "基隆市":"F-D0047-049","新竹市":"F-D0047-053","嘉義市":"F-D0047-057","臺北市":"F-D0047-061",
+        "高雄市":"F-D0047-065","新北市":"F-D0047-069","臺中市":"F-D0047-073","臺南市":"F-D0047-077",
+        "連江縣":"F-D0047-081","金門縣":"F-D0047-085"}
+    for name in api_list:
+        if name in address:
+            city_id = api_list[name]
+    t = time.time()
+    t1 = time.localtime(t+28800)
+    t2 = time.localtime(t+28800+10800)
+    now = time.strftime('%Y-%m-%dT%H:%M:%S',t1)
+    now2 = time.strftime('%Y-%m-%dT%H:%M:%S',t2)
+    url = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/{city_id}?Authorization={code}&elementName=WeatherDescription&timeFrom={now}&timeTo={now2}'
+    req = requests.get(url)   # 取得主要縣市預報資料
+    data = req.json()         # json 格式化訊息內容
+    location = data['records']['locations'][0]['location']
+    city = data['records']['locations'][0]['locationsName']
+    for item in location:
+        try:
+            area = item['locationName']
+            note = item['weatherElement'][0]['time'][0]['elementValue'][0]['value']
+            if not f'{city}{area}' in result:
+                result[f'{city}{area}'] = ''
+            else:
+                result[f'{city}{area}'] = result[f'{city}{area}'] + '。\n\n'
+            result[f'{city}{area}'] = result[f'{city}{area}'] + '未來三小時' + note
+        except:
+            pass
+
+    # 空氣品質
+    try:
+        url = 'https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON'
+        req = requests.get(url)
+        data = req.json()
+        records = data['records']
+        for item in records:
+            county = item['county']      # 縣市
+            sitename = item['sitename']  # 區域
+            name = f'{county}{sitename}'
+            aqi = int(item['aqi'])       # AQI 數值
+            aqi_status = ['良好','普通','對敏感族群不健康','對所有族群不健康','非常不健康','危害']
+            msg = aqi_status[aqi//50]    # 除以五十之後無條件捨去，取得整數
+
+            for k in result:
+                if name in k:
+                    result[k] = result[k] + f'\n\nAQI：{aqi}，空氣品質{msg}。'
+    except:
+        pass
+
+    output = '找不到氣象資訊'
+    for i in result:
+        if i in address: # 如果地址裡存在 key 的名稱
+            output = f'「{address}」{result[i]}'
+            break
+    return output
+
+def cctv(msg):
+    try:
+        output = ''
+        camera_list = {
+            '夢時代':'https://cctv1.kctmc.nat.gov.tw/27e5c086/',
+            '鼓山渡輪站':'https://cctv3.kctmc.nat.gov.tw/ddb9fc98/',
+            '中正交流道':'https://cctv3.kctmc.nat.gov.tw/166157d9/',
+            '五福愛河':'https://cctv4.kctmc.nat.gov.tw/335e2702/'
+        }
+        for item in camera_list:
+            if msg == item:
+                output = camera_list[msg]
     except Exception as e:
         print(e)
-        output = '抓取失敗...'
     return output
 
 # 主要的 LINE Bot 路由處理程式
@@ -108,13 +175,21 @@ def linebot():
                 text_message = TextSendMessage(text=reply[0])
                 line_bot_api.reply_message(reply_token,text_message)
                 line_bot_api.push_message(user_id, ImageSendMessage(original_content_url=reply[1], preview_image_url=reply[1]))
-            else:          
-                text_message = TextSendMessage(text=text)
-                line_bot_api.reply_message(reply_token,text_message)
+            else:
+                reply = cctv(text)
+                if not reply == '':
+                    text_message = TextSendMessage(text=reply)
+                    line_bot_api.reply_message(reply_token,text_message)
+                    sec = math.ceil(time.time())
+                    reply = reply + f'snapshot?t={sec}'
+                    line_bot_api.push_message(user_id, ImageSendMessage(original_content_url=reply, preview_image_url=reply))
+                else:
+                    text_message = TextSendMessage(text=text)
+                    line_bot_api.reply_message(reply_token,text_message)
         elif type == 'location':
             line_bot_api.push_message(user_id, TextSendMessage(text='馬上找給你！抓取資料中....'))
             address = json_data['events'][0]['message']['address'].replace('台','臺')  # 取出地址資訊，並將「台」換成「臺」
-            reply = weather(address)          
+            reply = weather(address)
             text_message = TextSendMessage(text=reply)
             line_bot_api.reply_message(reply_token,text_message)
     except Exception as e:
