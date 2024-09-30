d6c0ff7cec7f0a0d41dc3b132d23b577498418bb
taipeihugo <taipeihugo@users.noreply.github.com>
2024-09-30 23:40:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,17 +13,18 @@ app = Flask(__name__)
 # Channel Access Token and Channel Secret (從環境變數取得或自行設定)
 access_token = os.getenv('CHANNEL_ACCESS_TOKEN', '你的 Access Token')
 channel_secret = os.getenv('CHANNEL_SECRET', '你的 Channel Secret')
-weath_api = os.getenv('WEATHER_API', '你的 Weather API')
+weather_api = os.getenv('WEATHER_API', '你的 Weather API')
 
 # Line API 初始化
 line_bot_api = LineBotApi(access_token)
 handler = WebhookHandler(channel_secret)
 
+# 你的天氣 API 授權碼
+code = weather_api 
 
 # 地震查詢功能，整合中央氣象局地震資料的 API
 def earth_quake():
-    result = []
-    code = weath_api  # 你的天氣 API 授權碼
+    result = []     
     try:
         # 小區域地震
         url1 = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/E-A0016-001?Authorization={code}'
@@ -47,7 +48,38 @@ def earth_quake():
         print(e)
         result = ['抓取失敗...', '']  # 如果發生錯誤，返回失敗訊息
     return result
+    
+# 取得某個地點的氣象資訊
+def weather(address):
+    try:
+        url = [f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization={code}',
+            f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization={code}']
+        result = {}
+        for item in url:
+            req = requests.get(item)   # 爬取目前天氣網址的資料
+            data = req.json()
+            station = data['records']['Station']   # 觀測站
+            for i in station:
+                city = i['GeoInfo']['CountyName']  # 縣市
+                area = i['GeoInfo']['TownName']    # 區域
+                # 使用「縣市+區域」作為 key，例如「高雄市前鎮區」就是 key
+                # 如果 result 裡沒有這個 key，就記錄相關資訊
+                if not f'{city}{area}' in result:
+                    weather = i['WeatherElement']['Weather']
+                    temp = i['WeatherElement']['AirTemperature'] 
+                    humid = i['WeatherElement']['RelativeHumidity']
+                    # 回傳結果
+                    result[f'{city}{area}'] = f'目前天氣狀況「{weather}」，溫度 {temp} 度，相對濕度 {humid}%！'
 
+        output = '找不到氣象資訊'
+        for i in result:
+            if i in address: # 如果地址裡存在 key 的名稱
+                output = f'「{address}」{result[i]}'
+                break
+    except Exception as e:
+        print(e)
+        output = '抓取失敗...'
+    return output
 
 # 主要的 LINE Bot 路由處理程式
 @app.route("/", methods=['POST'])
@@ -63,7 +95,7 @@ def linebot():
 
         if type == 'text':
             text = json_data['events'][0]['message']['text']
-            if text == '雷達回波圖' or text == '雷達回波':
+            if text == '雷達':
                 line_bot_api.push_message(user_id, TextSendMessage(text='馬上找給你！抓取資料中....'))
                 img_url = f'https://cwaopendata.s3.ap-northeast-1.amazonaws.com/Observation/O-A0058-001.png?{time.time_ns()}'
                 img_message = ImageSendMessage(original_content_url=img_url, preview_image_url=img_url)
@@ -78,6 +110,12 @@ def linebot():
             else:
                 text_message = TextSendMessage(text=text)  # 回應接收到的其他文字訊息
                 line_bot_api.reply_message(reply_token, text_message)
+        elif type == 'location':
+            line_bot_api.push_message(user_id, TextSendMessage(text='馬上找給你！抓取資料中....'))
+            address = json_data['events'][0]['message']['address'].replace('台','臺')  # 取出地址資訊，並將「台」換成「臺」
+            reply = weather(address)          
+            text_message = TextSendMessage(text=reply)
+            line_bot_api.reply_message(reply_token,text_message)
     except Exception as e:
         print(e)  # 發生錯誤時印出錯誤訊息
     return 'OK'  # 驗證 Webhook 使用，不能省略
