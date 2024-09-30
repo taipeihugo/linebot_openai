c8e0b61c24fe52b029850704c6f566b5e6b73060
taipeihugo <taipeihugo@users.noreply.github.com>
2024-10-01 01:22:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -117,6 +117,7 @@ def weather(address):
             aqi = int(item['aqi'])       # AQI 數值
             aqi_status = ['良好','普通','對敏感族群不健康','對所有族群不健康','非常不健康','危害']
             msg = aqi_status[aqi//50]    # 除以五十之後無條件捨去，取得整數
+            print(f'name = {name}')
             print(f'\n\nAQI：{aqi}，空氣品質{msg}')
             for k in result:
                 if name in k:
