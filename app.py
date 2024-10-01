7ecc78368e6a22dde1748c6edead3104c82dc183
taipeihugo <taipeihugo@users.noreply.github.com>
2024-10-01 14:43:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,38 @@ handler = WebhookHandler(channel_secret)
 # 你的天氣 API 授權碼
 code = weather_api 
 
+headers = {'Authorization':'line_bot_api','Content-Type':'application/json'}
+body = {
+    'size': {'width': 2500, 'height': 640},    # 設定尺寸
+    'selected': 'true',                        # 預設是否顯示
+    'name': 'bbb',                             # 選單名稱
+    'chatBarText': 'b',                        # 選單在 LINE 顯示的標題
+    'areas':[                                  # 選單內容
+        {
+          'bounds': {'x': 0, 'y': 0, 'width': 1250, 'height': 640},           # 選單位置與大小
+          'action': {'type': 'uri', 'uri': 'https://line.me/R/nv/location/'}  # 點擊後開啟地圖定位，傳送位置資訊
+        },
+        {
+          'bounds': {'x': 1251, 'y': 0, 'width':625, 'height': 640},     # 選單位置與大小
+          'action': {'type': 'message', 'text':'雷達回波圖'}               # 點擊後傳送文字
+        },
+        {
+          'bounds': {'x': 1879, 'y': 0, 'width':625, 'height': 640},     # 選單位置與大小
+          'action': {'type': 'message', 'text':'地震資訊'}               # 點擊後傳送文字
+        }
+    ]
+  }
+# 向指定網址發送 request
+req = requests.request('POST', 'https://api.line.me/v2/bot/richmenu',
+                      headers=headers,data=json.dumps(body).encode('utf-8'))
+
+
+
+with open("line-bot-weather-demo.jpg", 'rb') as f:
+    line_bot_api.set_rich_menu_image("圖文選單 id", "image/jpeg", f)
+
+
+
 # 地震查詢功能，整合中央氣象局地震資料的 API
 def earth_quake():
     result = []     
